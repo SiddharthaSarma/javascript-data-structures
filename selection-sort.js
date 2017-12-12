df2db1516b09657fb0dfba43e30175a683dc0916
Siddhartha <sidduinevitable@gmail.com>
2017-12-12 14:58:35 +0530

    Adding selection sort

---
+++ selection-sort.js
@@ -0,0 +1,18 @@
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        let temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// test
+selectionSort([7, 5, 2, 4, 3, 9]); //[2, 3, 4, 5, 7, 9]
+selectionSort([9, 7, 5, 4, 3, 1]); //[1, 3, 4, 5, 7, 9]
+selectionSort([1, 2, 3, 4, 5, 6]); //[1, 2, 3, 4, 5, 6]
