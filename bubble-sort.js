7c83e398898516eb7a984810c05770968014102e
Siddhartha <sidduinevitable@gmail.com>
2017-12-12 14:51:23 +0530

    Adding bubble sort

---
+++ bubble-sort.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// test
+bubbleSort([7, 5, 2, 4, 3, 9]); //[2, 3, 4, 5, 7, 9]
+bubbleSort([9, 7, 5, 4, 3, 1]); //[1, 3, 4, 5, 7, 9]
+bubbleSort([1, 2, 3, 4, 5, 6]); //[1, 2, 3, 4, 5, 6]
