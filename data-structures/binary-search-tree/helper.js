21980db617c7f4629c047760072dd71a354f2d09
siddharthasarma <sidduinevitable@gmail.com>
2018-01-23 06:26:37 +0000

    Adding Helper.js file.

---
+++ data-structures/binary-search-tree/helper.js
@@ -0,0 +1,27 @@
+const Helper = (function () {
+
+  function insertData(tree) {
+    tree.insert(11);
+    tree.insert(7);
+    tree.insert(15);
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(9);
+    tree.insert(8);
+    tree.insert(10);
+    tree.insert(13);
+    tree.insert(12);
+    tree.insert(14);
+    tree.insert(20);
+    tree.insert(18);
+    tree.insert(25);
+    tree.insert(6);
+  }
+
+  return {
+    insertData: insertData
+  }
+
+})();
+
+module.exports = Helper;
