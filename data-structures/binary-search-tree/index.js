695347e35a040df0b7f1ada50478503ec583bcf4
siddharthasarma <sidduinevitable@gmail.com>
2018-01-23 05:21:12 +0000

    Added comments.

--- data-structures/binary-search-tree/index.js
+++ data-structures/binary-search-tree/index.js
@@ -11,6 +11,7 @@ function BinarySearchTree() {
   this.insert = function (data) {
     let newNode = new Node(data);
 
+    // checking if root exists or not
     if (this.root === null) {
       this.root = newNode;
     }
@@ -20,7 +21,11 @@ function BinarySearchTree() {
   }
 
   const insertNode = function (node, newNode) {
+    // If the data is less than existing node then it should be aligned left
     if (newNode.data < node.data) {
+
+      // If left node doesn't exist then create it
+      // otherwise add left node to the existing node.
       if (node.left === null) {
         node.left = newNode
       }
@@ -29,7 +34,11 @@ function BinarySearchTree() {
       }
     }
 
+    // If the data is greater than existing node then it should be aligned right
     if (newNode.data > node.data) {
+
+      // If right node doesn't exist then create it
+      // otherwise add right node to the existing node.
       if (node.right === null) {
         node.right = newNode
       }
