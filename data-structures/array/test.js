ade24f4d99a810d7c4958afcb565dfc662dbc129
siddharthasarma <sidduinevitable@gmail.com>
2018-01-25 18:16:48 +0530

    Updated the testcases

--- data-structures/array/test.js
+++ data-structures/array/test.js
@@ -12,8 +12,17 @@ describe('Array data-structure testcases', () => {
     arr.add(2);
     arr.add(3);
     arr.add(4);
+    expect(arr.length()).toEqual(4);
+  });
+
+  test('it should return all the elements present', () => {
+    let arr = new MyArray();
+    arr.add(1);
+    arr.add(2);
+    arr.add(3);
+    arr.add(4);
     let result = [];
     arr.print(el => result.push(...el));
-    expect(result.length).toEqual(4);
+    expect(result).toEqual([1, 2, 3, 4]);
   });
 });
