302133794b2808234acd0beddf8e6ce562541168
siddharthasarma <sidduinevitable@gmail.com>
2018-01-25 17:48:46 +0530

    Formatted the code to make it easier for unit testcases

--- data-structures/array/index.js
+++ data-structures/array/index.js
@@ -35,24 +35,8 @@ MyArray.prototype.length = function() {
   return this.items.length;
 };
 
-MyArray.prototype.print = function() {
-  console.log(this.items.join(' '));
+MyArray.prototype.print = function(cb) {
+  cb(this.items);
 };
 
-// test
-var array = new MyArray();
-array.add(1);
-array.add(2);
-array.add(3);
-array.add(4);
-array.print(); // => 1 2 3 4
-console.log('search 3 gives index 2:', array.search(3)); // => 2
-console.log('getAtIndex 2 gives 3:', array.getAtIndex(2)); // => 3
-console.log('length is 4:', array.length()); // => 4
-array.remove(3);
-array.print(); // => 1 2 4
-array.add(5);
-array.add(5);
-array.print(); // => 1 2 4 5 5
-array.remove(5);
-array.print(); // => 1 2 4
+module.exports = MyArray;
