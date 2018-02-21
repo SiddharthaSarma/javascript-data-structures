264d41ab78d6cd96c03982647eb5f0865b2c2d31
Siddhartha <sidduinevitable@gmail.com>
2018-02-21 12:33:14 +0530

    Adding one more approach in insertion sort

--- sorting/insertion-sort.js
+++ sorting/insertion-sort.js
@@ -23,5 +23,20 @@ function swap(arr, i, j) {
   arr[j] = temp;
 }
 
+// second approach
+function insertionSort(a) {
+  for (let i = 0; i < a.length; i++) {
+    let temp = a[i];
+    let j = i;
+    while (j > 0 && temp < a[j - 1]) {
+      a[j] = a[j - 1];
+      j -= 1;
+    }
+    a[j] = temp;
+  }
+  console.log(a);
+}
+
+
 // test
 insertionSort([7, 1, 2, 4, 3, 9]); // [1, 2, 3, 4, 7, 9]
